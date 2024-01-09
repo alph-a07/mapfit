3f35838c914bc0ea6bedbd7e0cdef59e8e2d2127
Jeel <itsjeel01@gmail.com>
2024-01-09 13:58:54 +0530

    Added map marking feature with popup

--- script.js
+++ script.js
@@ -10,3 +10,53 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map, mapEvent;
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const coords = [position.coords.latitude, position.coords.longitude];
+
+            // map => ID of the target view
+            // 13 => Initial zoom level
+            map = L.map('map').setView(coords, 15);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(map);
+
+            map.on('click', function (e) {
+                form.classList.remove('hidden');
+                mapEvent = e;
+                inputDistance.focus();
+            });
+        },
+        function () {
+            alert('Please grant location access!');
+        }
+    );
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const { lat, lng } = mapEvent.latlng;
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+            L.popup({ minWidth: 100, maxWidth: 250, autoClose: false, closeOnClick: false, className: 'running-popup' }).setContent('Workout!')
+        )
+        .openPopup();
+
+    inputDistance.value = '';
+    inputDuration.value = '';
+    inputCadence.value = '';
+    inputElevation.value = '';
+});
+
+// Change the cadence/elevation depending on workout type
+inputType.addEventListener('change', function () {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
